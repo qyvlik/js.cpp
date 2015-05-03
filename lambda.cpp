e04b79c6416dbe749791c6d5c3b6984368372225
qyvlik <1403085871@QQ.COM>
2015-05-03 17:24:09 +0800

    lambda and auto in cpp

---
+++ lambda.cpp
@@ -0,0 +1,26 @@
+/*
+ * -std+=c++11 or -std+=c++13
+*/
+#include <iostream>
+using namespace std;
+
+#define var auto
+#define function [&]
+
+class SampleTest
+{
+public:
+    void action(){
+        cout <<"sample Test action"<<endl;
+    }
+};
+
+int main()
+{
+    var foo = function(){
+        return SampleTest();
+    };
+    var g = foo();
+    g.action();
+}
+
